3f30c5919465b1eb7e3dd3d076d5c314d1c51536
iambean-git <peb1021@naver.com>
2024-09-10 12:42:13 +0900

    박스오피스

--- 12/12.js
+++ 12/12.js
@@ -1,30 +1,57 @@
-//데이터 가져오기
-const getData = (ul) => {
-    console.log('getData');
-    let url = 'https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=82ca741a2844c5c180a208137bb92bd7&targetDt=20240908';
+//OPEN API 데이터 가져오기
+const getData = (selDt, ul) =>{
+    console.log(selDt);
+    const testAPI = '82ca741a2844c5c180a208137bb92bd7';
+    let url = `https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?`;
+    url = `${url}key=${testAPI}&targetDt=${selDt}`;
+    console.log(url);   //URL 확인
 
     fetch(url)
-        .then( resp => resp.json() )
-        
-        //.then( data => console.log(data.boxOfficeResult.dailyBoxOfficeList) )
-        //(또는 이렇게도 가능).then( data => console.log(data.boxOfficeResult['dailyBoxOfficeList']) )
-
-        .then( data => {
+        .then( resp => resp.json())
+        .then( data=> {
             console.log(data.boxOfficeResult.dailyBoxOfficeList);
             let dailyBoxOfficeList = data.boxOfficeResult.dailyBoxOfficeList;
 
-            let tm = dailyBoxOfficeList.map(item => `<li>${item.movieNm}</li>`)
-            console.log(tm);
-
+            let tm = dailyBoxOfficeList.map(item => 
+                `<li class="mvli">
+                    <span class = "rank"> ${item.rank} </span>
+                    <span class ="movieNM"> ${item.movieNm} </span>
+                </li>`);
             ul.innerHTML = tm.join('');
+            ul.style.border = '1px dotted rgb(177, 177, 177)';
         })
-
-        .catch( err => console.error(err)) ;
+        .catch( err => console.error(err));
 }
 
-document.addEventListener('DOMContentLoaded', ()=>{
-    //요소 가져오기
-    const ul = document.querySelector('.sec>ul');
-    getData(ul);
+//어제 날짜 구하기 함수
+const getYesterday = () => {
+    const yesterday = new Date();
+    yesterday.setDate(yesterday.getDate() -1);
+    //console.log('yesterday : ', yesterday );
 
+    let dateString = '';
+    dateString = dateString + yesterday.getFullYear() +"-"
+                +(("0" +(yesterday.getMonth()+1))).slice(-2) + "-"
+                +("0" +yesterday.getDate()).slice(-2)
+    //console.log("hello", dateString);
+    return dateString;
+}
+
+
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    //요소가져오기
+    const dt = document.querySelector('#dt');
+    const ul = document.querySelector('.sec>ul');
+    //어제날짜구하기
+    let yesterday = getYesterday();
+    console.log('yesterday : ', yesterday );
+
+    //date요소 최댓값 설정
+    dt.max = yesterday;
+
+    dt.addEventListener('change', ()=>{
+        getData(dt.value.replaceAll('-',''), ul);
+    });
+    
 });
